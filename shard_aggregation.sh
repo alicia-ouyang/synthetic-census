2d9ccc65c1a479f372efde979aca7702fa3cacac
aliciao <aliciao@orcd-login001.mit.edu>
2025-09-07 17:14:14 -0400

    Updated all shell scripts to r8

--- shard_aggregation.sh
+++ shard_aggregation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 1                # Number of cores (-c)
 #SBATCH -t 0-0:15          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p sched_mit_sloan_batch # Partition to submit to
+#SBATCH -p sched_mit_sloan_batch_r8 # Partition to submit to
 #SBATCH --mem=1000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_files/samp.%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e out_files/samp.%j.err  # File to which STDERR will be written, %j inserts jobid
@@ -13,5 +13,7 @@ else
     echo Missing arguments PARAM_FILE or TASK_NAME
     exit 1
 fi
-module load sloan/python/modules/python-3.6/gurobipy/9.0.1
+module load miniforge
+module load gurobi/12.0.3
+source activate synthetic-census
 python3 aggregate_data_shards.py --from_params "$PARAM_FILE" --task_name "$TASK_NAME"
--- single_shard_generation.sh
+++ single_shard_generation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 12                # Number of cores (-c)
 #SBATCH -t 0-10:00          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p sched_mit_sloan_batch   # Partition to submit to
+#SBATCH -p sched_mit_sloan_batch_r8   # Partition to submit to
 #SBATCH --mem=4000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_files/census.%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e out_files/census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
@@ -15,6 +15,7 @@ else
     echo Missing arguments PARAM_FILE TASK_NAME TASK_NUM TASKS
     exit 1
 fi
-module load sloan/python/modules/python-3.6/gurobipy/9.0.1
-python3 -m pip install gurobipy
+module load miniforge
+module load gurobi/12.0.3
+source activate synthetic-census
 python3 generate_data_shard.py --from_params "$PARAM_FILE" --task $TASK_NUM --num_tasks $TASKS --task_name $TASK_NAME

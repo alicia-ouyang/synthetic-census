59e35c13447a4818f93a5c1a1779e9ff103237e6
aliciao <aliciao@orcd-login001.mit.edu>
2025-09-11 11:57:18 -0400

    updating number of runs

--- shard_generation.sh
+++ shard_generation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -c 4                # Number of cores (-c)
-#SBATCH -t 0-10:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-16:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p sched_mit_sloan_batch_r8   # Partition to submit to
 #SBATCH --mem=5000           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o out_files/census.%A_%a.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e out_files/census.%A_%a.err  # File to which STDERR will be written, %j inserts jobid
-#SBATCH --array=1-100
+#SBATCH --array=1-400
 #SBATCH --mail-type=END
 if [ "$#" -eq 2 ]; then
     PARAM_FILE="$1"
